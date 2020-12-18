29661c50f665c3e47059ef2db7acd00da3bf7707
UttamkiniH <53141266+UttamkiniH@users.noreply.github.com>
2020-12-18 16:22:24 +0530

    Create samosa.kt

---
+++ samosa.kt
@@ -0,0 +1,20 @@
+//without repeat function
+// variable and how to use them, comment
+fun main() {
+    val num = 7+6
+    val name="john"
+    print("My name ")
+    print("is ${name} ") // to comment
+    println("and I like to eat ${num} samosa") //binod likes 10 samosa
+    println("Your Samosa\n") 
+    println("                 -/")
+    println("                -/++")
+    println("              --/++++")
+    println("            ---/++++++ ")
+    println("           ---/++++++++ ")
+    println("         ----/++++++++++ ")
+    println("        ----/++++++++++++ ")
+    println("      -----/+++++++++++++++ ")
+    println("     -----/+++++++++++++++++ ")
+    println("    -----/+++++++++++++++++++ ")
+}
